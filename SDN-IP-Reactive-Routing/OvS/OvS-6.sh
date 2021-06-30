21219725d5081910f64c4b8b5463156478e661fb
donnyao <ao.donny@gmail.com>
2021-06-30 14:05:28 +0700

    Updated OVS configuration

---
+++ SDN-IP-Reactive-Routing/OvS/OvS-6.sh
@@ -0,0 +1,9 @@
+tee -a /etc/network/interfaces << END
+auto eth0
+iface eth0 inet static
+  address 12.20.0.6
+  netmask 255.255.255.0
+END
+ovs-vsctl del-port br0 eth0
+ovs-vsctl set-controller br0 tcp:12.20.0.123:6633
+ovs-vsctl set bridge br0 stp_enable=true
