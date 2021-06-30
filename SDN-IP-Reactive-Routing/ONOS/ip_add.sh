503bbf6dad3cd1249c3f6f22d7605713c4c149ec
donnyao <ao.donny@gmail.com>
2021-06-30 13:59:59 +0700

    Create ip_add.sh

---
+++ SDN-IP-Reactive-Routing/ONOS/ip_add.sh
@@ -0,0 +1,13 @@
+sudo tee -a /etc/network/interfaces << END
+auto ens3
+iface ens3 inet dhcp
+auto ens4
+iface ens4 inet static
+  address 12.20.0.123
+  netmask 255.255.255.0
+auto ens5
+iface ens5 inet static
+  address 172.12.10.1
+  netmask 255.255.255.252
+END
+sudo systemctl restart networking
